55e9937180d7339d58de6c84c0df55ae902d6807
mrgick <mrgickcool@gmail.com>
2024-08-13 21:50:03 +0300

    Add clean up (tests)

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ import pytest
 from httpx import ASGITransport, AsyncClient
 from models.media_file import Base
 
+from src.config import settings
 from src.database import engine
 from src.main import app
 
@@ -19,7 +20,23 @@ def anyio_backend(request):
 
 
 @pytest.fixture(scope="session")
+def empty_directory_path():
+    for file in settings.directory_path.iterdir():
+        if file.is_file():
+            file.unlink()
+    yield
+    for file in settings.directory_path.iterdir():
+        if file.is_file():
+            file.unlink()
+
+
+@pytest.fixture(scope="session")
 async def start_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    await engine.dispose()
+    yield
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
@@ -27,7 +44,7 @@ async def start_db():
 
 
 @pytest.fixture(scope="session")
-async def client(start_db) -> AsyncGenerator[AsyncClient, None]:
+async def client(start_db, empty_directory_path) -> AsyncGenerator[AsyncClient, None]:
 
     transport = ASGITransport(
         app=app,
